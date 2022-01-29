fb6ffbbc57d81cedf9b92884a6e1aeba20ca58c7
umut9191 <umutsurmeli91@gmail.com>
2022-01-29 17:35:48 +0300

    Open-Closed principle example codes added

---
+++ OCP/OCP.swift
@@ -0,0 +1,84 @@
+//
+//  OCP.swift
+//  OCP
+//
+//  Created by Mac on 28.01.2022.
+//
+//Open-Close Prinsible is close for modification and open for extensible
+import Foundation
+enum Color {
+    case red
+    case green
+    case blue
+}
+enum Size {
+    case small
+    case medium
+    case large
+    case huge
+}
+class Product{
+    var name: String
+    var color: Color
+    var size : Size
+    
+    init(_ name:String,_ color: Color,_ size: Size) {
+        self.name = name
+        self.color = color
+        self.size = size
+    }
+}
+protocol Specification {
+    associatedtype T
+    func isSatisfied(_ item: T) ->Bool
+}
+protocol Filter {
+    associatedtype T
+    func filter<Spec: Specification>(_ items: [T],_ spec: Spec) -> [T]
+    where Spec.T == T
+}
+class ColorSpecification: Specification {
+    typealias T = Product
+    let color : Color
+    init(_ color :Color) {
+        self.color = color
+    }
+    func isSatisfied(_ item: Product) -> Bool {
+        return item.color == color
+    }
+}
+class SizeSpecification: Specification {
+    typealias T = Product
+    let size : Size
+    init(_ size :Size) {
+        self.size = size
+    }
+    func isSatisfied(_ item: Product) -> Bool {
+        return item.size == size
+    }
+}
+class AndSpecification<T,SpecA:Specification,SpecB:Specification> : Specification
+where SpecA.T == SpecB.T, T == SpecA.T , T == SpecB.T{
+    let first : SpecA
+    let second : SpecB
+    init(_ first: SpecA, _ second : SpecB) {
+        self.first = first
+        self.second = second
+    }
+    func isSatisfied(_ item: T) -> Bool {
+        return first.isSatisfied(item) && second.isSatisfied(item)
+    }
+}
+class BetterFilter: Filter {
+typealias T = Product
+    func filter<Spec:Specification>(_ items: [Product], _ spec: Spec) -> [Product] where  T == Spec.T {
+        var result = [Product]()
+        for i in items{
+            if spec.isSatisfied(i) {
+                result.append(i)
+            }
+        }
+        return result
+    }
+}
+
--- OCP/main.swift
+++ OCP/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+func oCP(){
+    let apple = Product("Apple", .green,.small)
+    let tree = Product ("Tree", .green,.large)
+    let house = Product("House",.blue,.large)
+    
+    let products = [apple,tree,house]
+    
+    let bf = BetterFilter()
+    print("Green Products;")
+    for p  in bf.filter(products, ColorSpecification(.green)) {
+        print("\(p.name)  is grren")
+    }
+    
+    print("large Blue Products;")
+    for p  in bf.filter(products, AndSpecification(ColorSpecification(.blue),SizeSpecification(.large))) {
+        print("\(p.name)  is blue and large ")
+    }
+}
 
+oCP()
